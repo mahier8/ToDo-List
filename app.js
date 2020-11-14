418c963167a6b045496ac1bc0bf23b4e524dd326
Mahier <mahier.sydow8@gmail.com>
2020-11-14 19:11:11 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event listeners
+todoButton.addEventListener('click', addToDo);
+
+// Functions 
+function addToDo (event) {
+    //prevent form from submitting
+    event.preventDefault();
+    // Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo); 
+    // check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear todo input value
+    todoInput.value = '';
+}
